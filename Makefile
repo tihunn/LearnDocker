99d2e437bbc568e5186504c97212e0ce641b9d9f
tihunn <74502721+tihunn@users.noreply.github.com>
2023-02-19 17:54:26 +0300

    push files

---
+++ Makefile
@@ -0,0 +1,14 @@
+mongo:
+	docker run -p 27017:27017 -d --rm --name mongodb --network notes-net -v mongo-data:/data/db --env-file ./config/development.evn mongo
+
+server:
+    docker run -d -p 5000:5000 -d --rm --name nodetest --network notes-net --env-file ./config/development.evn -v C:/docker-mern/docker-mern/server:/app -v /app/node_modules nodetest
+
+client:
+    docker run -d -p 3000:3000 -d --rm --name fronttest -v C:/docker-mern/docker-mern/client/src:/app/src --env-file ./config/development.evn fronttest
+
+dev:
+    docker-compose -f docker-compose.yml up -d
+
+build:
+    docker-compose -f docker-compose.production.yml up
---
+++ client/src/App.js
@@ -0,0 +1,39 @@
+import {useCallback, useEffect, useState} from 'react'
+import axios from 'axios'
+import {List} from './List'
+import {Form} from './Form'
+
+function App() {
+  const [notes, setNotes] = useState([])
+
+  const baseURL = "http://localhost:5000"
+  const api = axios.create({baseURL})
+
+  async function createNote(text) {
+    const note = await api.post('/api/note', {text})
+    setNotes([...notes, {...note.data.note}])
+  }
+
+  const fetchNotes = useCallback(async () => {
+    const notes = await api.get('/api/note')
+    setNotes(notes.data)
+  }, [])
+
+  useEffect(() => {
+    fetchNotes()
+  }, [fetchNotes])
+
+  return (
+    <>
+      <nav className="navbar">
+        <h3>Docker MERN</h3>
+      </nav>
+      <div className="container with-nav">
+        <Form onCreate={createNote} />
+        <List list={notes} />
+      </div>
+    </>
+  )
+}
+
+export default App;
